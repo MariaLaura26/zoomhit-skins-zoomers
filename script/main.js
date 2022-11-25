029d82450b379f30d0c00fd1a154894698d8808b
Natacha-A <90852900+Natacha-A@users.noreply.github.com>
2022-11-25 13:59:48 -0500

    js début

--- script/main.js
+++ script/main.js
@@ -1,8 +1,14 @@
 
+//Boucler sur les nouvelles
+let nouvellesArr = document.querySelector(".nouvelle");
+
 //Quand tu click sur une image ou un bouton d'une nouvelle dans les nouvelles: Afficher la page de la nouvelle
 let btnlienItem = document.querySelector(".lienItem");
 let btnLienItemP = document.querySelector(".lienItemP");
 
-btnlienItem.addEventListener('click', function() {
-    console.log("hello");
+nouvellesArr.array.forEach(nouvelle => {
+    nouvelle.addEventListener('click', function() {
+        console.log("hello");
+    });
 });
+    
