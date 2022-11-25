120bb3903b4f8f4f8579e589c9201e32f8941ba4
Natacha-A <90852900+Natacha-A@users.noreply.github.com>
2022-11-25 13:41:03 -0500

    bootstrap et js

---
+++ script/main.js
@@ -0,0 +1,8 @@
+
+//Quand tu click sur une image ou un bouton d'une nouvelle dans les nouvelles: Afficher la page de la nouvelle
+let btnlienItem = document.querySelector(".lienItem");
+let btnLienItemP = document.querySelector(".lienItemP");
+
+btnlienItem.addEventListener('click', function() {
+    console.log("hello");
+});
